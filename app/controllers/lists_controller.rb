8e74391d4d37c62ab973b038098dee8b648ce816
reuben frith <reubenfrith007@gmail.com>
2022-11-15 22:22:39 +1100

    created list controller and methods

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show create]
+
+
+  def index
+    @list = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(task_params)
+    @list.save
+    redirect_to '/lists'
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Show all lists
+  get 'lists', to: 'lists#index'
+  # Add new movie to list - Create
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  # Show single list
+  get 'lists/:id', to: 'lists#show', as: 'list'
 end
