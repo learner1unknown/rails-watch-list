8114531c874baf31afef0f6af129a9a438e84ab1
reuben frith <reubenfrith007@gmail.com>
2022-11-15 21:32:44 +1100

    models done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  # validates_uniqueness_of :list_id, :scope => [:movie_id]
   validates :movie, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
 end
